912d244f804bc3217d34e42c0d3657c41cc97f4a
dot-D69 <anuragvishwas19@gmail.com>
2025-02-15 03:33:39 +0530

    Pass walletAddress, sanityTokens, and thirdWebTokens props to Portfolio component in Main for enhanced data handling

--- components/Main.js
+++ components/Main.js
@@ -3,21 +3,24 @@ import Portfolio  from './Portfolio'
 import styled from 'styled-components'
 import Promos from './Promo'
 
-const Main = () =>{
+const Main = ({thirdWebTokens, sanityTokens, walletAddress}) =>{
     return(
     <Wrapper>
-        <Portfolio />
+        <Portfolio walletAddress={walletAddress} sanityTokens={sanityTokens} thirdWebTokens= {thirdWebTokens}  />
         <Promos/>
     </Wrapper>
     )
 }
-
 export default Main
 
 const Wrapper = styled.div`
   display: flex;
   max-height: calc(100vh - 64px);
   overflow: auto;
+
+  // ::-webkit-scrollbar{
+  //     display: none;
+  // }
 
   & div {
     border-radius: 0.4rem;
